41e5cd20650024343e3bc6f825f35109757afd98
BlackBracken <me@bracken.black>
2019-02-11 07:35:49 +0900

    add name & profile

---
+++ assets/js/description.js
@@ -0,0 +1,9 @@
+ityped.init("#description", {
+    strings: ["a.k.a. Fumitaka, Yoshikane", "Like reading, gaming, and coding."],
+    typeSpeed: 70,
+    backSpeed: 25,
+    startDelay: 300,
+    backDelay: 1200,
+    showCursor: false,
+    loop: true
+});
