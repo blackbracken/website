80f19688f9e3372bd2d30c63a6f7fa1eaaa94b8e
BlackBracken <me@bracken.black>
2019-02-11 07:37:16 +0900

    hide me

--- assets/js/description.js
+++ assets/js/description.js
@@ -1,5 +1,5 @@
 ityped.init("#description", {
-    strings: ["a.k.a. Fumitaka, Yoshikane", "Like reading, gaming, and coding."],
+    strings: ["Like reading, gaming, and coding."],
     typeSpeed: 70,
     backSpeed: 25,
     startDelay: 300,
