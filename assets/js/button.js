e8ce89ed3f9471c116f8036c35a01170ad511ef8
BlackBracken <me@bracken.black>
2019-02-16 05:38:55 +0900

    add button on mail modal to open mailer

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,22 +1,28 @@
-const LINK_GITHUB = "https://github.com/blackbracken/";
-const LINK_TWITTER = "https://twitter.com/black_bracken/";
-const LINK_BLOG = "https://blackbracken.hatenablog.com/";
+const MAIL = 'me(at_mark)bracken.black'.replace('(at_mark)', '@');
+const LINK_GITHUB = 'https://github.com/blackbracken/';
+const LINK_TWITTER = 'https://twitter.com/black_bracken/';
+const LINK_BLOG = 'https://blackbracken.hatenablog.com/';
 
-let mail_modal = new tingle.modal();
-mail_modal.setContent('<h2>me(at_mark)bracken.black</h2>'.replace("(at_mark)", "@"));
+let mail_modal = new tingle.modal({
+    footer: true
+});
+mail_modal.setContent('<h2>' + MAIL + '</h2>');
+mail_modal.addFooterBtn('Send me a mail', 'tingle-btn tingle-btn--primary tingle-btn--pull-right', () => {
+    location.href = 'mailto:' + MAIL;
+});
 
-document.getElementById("github-button").onclick = () => {
+document.getElementById('github-button').onclick = () => {
     window.open(LINK_GITHUB);
 };
 
-document.getElementById("twitter-button").onclick = () => {
+document.getElementById('twitter-button').onclick = () => {
     window.open(LINK_TWITTER);
 };
 
-document.getElementById("blog-button").onclick = () => {
+document.getElementById('blog-button').onclick = () => {
     window.open(LINK_BLOG);
 };
 
-document.getElementById("mail-button").onclick = () => {
+document.getElementById('mail-button').onclick = () => {
     mail_modal.open()
 };
