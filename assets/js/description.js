a356dbb785666ee941a0dfbe47d5a3ac06c38e83
BlackBracken <me@bracken.black>
2019-05-07 15:58:33 +0900

    remove animate description

--- assets/js/description.js
+++
@@ -1,9 +0,0 @@
-ityped.init("#description", {
-    strings: ["Like reading, gaming, and coding."],
-    typeSpeed: 70,
-    backSpeed: 25,
-    startDelay: 300,
-    backDelay: 1200,
-    showCursor: false,
-    loop: true
-});
